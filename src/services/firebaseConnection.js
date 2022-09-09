277e56d1eaf8bf2ec43ca74c18fd08959e51a674
Alessandro <56701291+Alessandro021@users.noreply.github.com>
2022-09-08 22:23:48 -0300

    Update firebaseConnection.js

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -3,12 +3,12 @@ import 'firebase/auth'
 import 'firebase/database'
 
 let firebaseConfig = {
-    apiKey: "AIzaSyDJAOi11wNo33F_YYDz3sbHHn9zqlPd78M",
-    authDomain: "financas-542cc.firebaseapp.com",
-    projectId: "financas-542cc",
-    storageBucket: "financas-542cc.appspot.com",
-    messagingSenderId: "417345075941",
-    appId: "1:417345075941:web:94954d8d0e24e290a3b20c"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   
   if(!firebase.apps.length){
